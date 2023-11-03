919885807b978fbc064c6991bad2e8adb70b4eb9
scottishwidow <mike.voznyanskix@gmail.com>
2023-11-03 20:24:47 +0200

    modified:   server.py

--- server.py
+++ server.py
@@ -3,10 +3,13 @@ import socketserver
 
 PORT = 8080
 Handler = http.server.SimpleHTTPRequestHandler
+Hostname = '127.0.0.1'
 
 import os
 os.chdir('./html')
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving at port {PORT}")
+    print(f"Serving at http://{Hostname}:{PORT}")
     httpd.serve_forever()
+
+
