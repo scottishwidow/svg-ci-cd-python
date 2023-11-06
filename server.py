fa530ece22e8595952a81640f0e21ad652fa5d58
scottishwidow <mike.voznyanskix@gmail.com>
2023-11-06 13:33:32 +0200

    modified:   server.py

--- server.py
+++ server.py
@@ -5,11 +5,14 @@ PORT = 8080
 Handler = http.server.SimpleHTTPRequestHandler
 Hostname = '127.0.0.1'
 
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+        super().end_headers()
+
 import os
 os.chdir('./html')
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
     print(f"Serving at http://{Hostname}:{PORT}")
     httpd.serve_forever()
-
-
